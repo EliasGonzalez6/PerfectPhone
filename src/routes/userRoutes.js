62c4048cb152c0ce0069ec1351b31e4ffa0a00d9
Elias <moriancumer6@gmail.com>
2021-09-21 01:24:32 -0300

    comienzo usuario

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -3,9 +3,13 @@ const router = express.Router();
 
 const usersController = require('../controllers/userController')
 
-
+//formulario de ingreso
 router.get("/ingreso",usersController.formularioUsuario);
 
+//formulario de registro
+router.get("/registro",usersController.formularioRegistro);
+
+//procesar registro
 router.get("/registro",usersController.formularioRegistro);
 
 router.get("/perfil/:userId",usersController.perfilUsuario);
