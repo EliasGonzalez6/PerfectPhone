b4d097c50b86012e526a83cb48b8dc610806cfb2
wendy <wendy.torres.19ee@gmail.com>
2021-10-02 17:57:29 -0300

    Model DB Product

---
+++ src/database/models/Product.js
@@ -0,0 +1,73 @@
+module.exports = function(sequelize, dataTypes){
+    let alias = "Product";
+
+    let cols = {
+        id: {
+            type: dataTypes.INTEGER(11),
+            primaryKey: true,
+            allowNull:false,
+            autoIncrement: true
+        },
+        name: {
+            type: dataTypes.STRING(50),
+            allowNull:false
+        },
+        description: {
+            type: dataTypes.STRING(100),
+            allowNull:true
+        },
+        image: {
+            type: dataTypes.STRING(100),
+            allowNull:true
+        },
+        price: {
+            type: dataTypes.DECIMAL(10,4),
+            allowNull:false
+        },
+        stock: {
+            type: dataTypes.INTEGER(11),
+            allowNull:false
+        },
+        color: {
+            type: dataTypes.STRING(20),
+            allowNull:true
+        },
+        brand: {
+            type: dataTypes.INTEGER(11),
+            allowNull:true,
+            unique:true
+        },
+        category: {
+            type: dataTypes.INTEGER(11),
+            allowNull:true,
+            unique:true
+        }
+    }
+
+    let config = {
+        tableName: "products",
+        timestamps: false
+    }
+
+    let Product = sequelize.define(alias, cols, config);
+
+    Product.associate = (models) => {
+        
+        Product.hasmany(models.Saledetails, {
+            as:"saledetails",
+            foreignKey:"product"
+        }),
+
+        Product.belongsTo(models.Brand, {
+            as:"products",
+            foreignKey:"brand"
+        }),
+
+        Product.belongsTo(models.Category, {
+            as:"products",
+            foreignKey:"category"
+        })
+    }
+
+    return Product;
+}
