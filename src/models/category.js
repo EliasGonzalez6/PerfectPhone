ea101890f2bf9abef5d64f684cbb5aa6201a033e
Elias <moriancumer6@gmail.com>
2021-10-21 20:57:28 -0300

    juntos en el git hut

--- src/models/category.js
+++
@@ -1,9 +0,0 @@
-const Sequelize = require("sequelize")
-const { Op } = Sequelize;
-const db = require ("../database/models");
-const {Category} = db
-
-const model = {
-    
-};
-module.exports = model;
